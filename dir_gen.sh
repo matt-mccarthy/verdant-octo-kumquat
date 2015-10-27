6f1112f673eecf8b764f12b5e78368ca1fa5b6ef
Matt McCarthy <matthew.mccarthy.12@cnu.edu>
2015-10-26 22:46:16 -0400

    Added directory generator

---
+++ dir_gen.sh
@@ -0,0 +1,20 @@
+modulus=$1
+dir=$2
+first=$3
+last=$4
+
+(mkdir $dir) 2> /dev/null
+
+for i in `seq $first $last`
+do
+	j=$(($i % $modulus))
+	
+	k_ones=$(($j % 10))
+	k_tens=$(( $(($j/10)) % 10 ))
+	k_huns=$(( $(($j/100)) % 10 ))
+	
+	(mkdir $dir/$k_huns $dir/$k_huns/$k_tens $dir/$k_huns/$k_tens/$k_ones) 2> /dev/null
+	touch $dir/$k_huns/$k_tens/$k_ones/$i.txt
+	
+	dd if=/dev/urandom of=$dir/$k_huns/$k_tens/$k_ones/$i.txt bs=512 count=28
+done
