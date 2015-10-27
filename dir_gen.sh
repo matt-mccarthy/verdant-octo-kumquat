6296dbdde8b97b1061b22e430278a0b0faff19b8
Matt McCarthy <matthew.mccarthy.12@cnu.edu>
2015-10-26 22:58:57 -0400

    Made the directory generator better

--- dir_gen.sh
+++ dir_gen.sh
@@ -2,6 +2,7 @@ modulus=$1
 dir=$2
 first=$3
 last=$4
+source=$5
 
 (mkdir $dir) 2> /dev/null
 
@@ -16,5 +17,5 @@ do
 	(mkdir $dir/$k_huns $dir/$k_huns/$k_tens $dir/$k_huns/$k_tens/$k_ones) 2> /dev/null
 	touch $dir/$k_huns/$k_tens/$k_ones/$i.txt
 	
-	dd if=/dev/urandom of=$dir/$k_huns/$k_tens/$k_ones/$i.txt bs=512 count=28
+	(dd if=$source of=$dir/$k_huns/$k_tens/$k_ones/$i.txt bs=512 count=28) 2> /dev/null
 done
