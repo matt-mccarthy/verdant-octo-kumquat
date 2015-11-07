f12a3bd588bd58569545f4f1897f286b415565f6
Matt McCarthy <matthew.mccarthy.12@cnu.edu>
2015-11-06 19:18:38 -0500

    Almost done with cache

--- cache.cpp
+++ cache.cpp
@@ -1,6 +1,7 @@
-#include <algorithm>
+#include <atomic>
 #include <chrono>
 #include <condition_variable>
+#include <fstream>
 #include <mutex>
 #include <queue>
 #include <string>
@@ -9,92 +10,149 @@
 
 #include "cache.h"
 
-using std::fill_n;
+using std::atomic;
+using std::atomic_bool;
+using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::system_clock;
 using std::condition_variable;
 using std::mutex;
-using std::queue;
+using std::priority_queue;
+using std::string;
 using std::thread;
+using std::unique_lock;
 using std::unordered_map;
 
-typedef unordered_map<int><int>	db_map;
-typedef unordered_map<int><cache::entry> c_map;
+typedef unordered_map<int, int>	db_map;
+typedef unique_lock<mutex> slock;
 
-cache::cache(db_map& mapper, string& db_location, unsigned entry_length,
-				unsigned entries_per_line, unsigned num_lines,
-				unsigned num_threads)
-			: db_mapper(mapper), db_loc(db_location), db_file(db_loc),
+cache::cache(db_map& mapper, string& db_location, int* id_list,
+				unsigned entry_length, unsigned entries_per_line,
+				unsigned num_lines, unsigned num_threads)
+			: db_file(db_location),
 				entry_size(entry_length), line_length(entries_per_line),
 				line_count(num_lines), thread_count(num_threads),
 				cache_size(line_count*line_length),
-				stop(false), reader(read), fetch_count(0) {}
+				stop(false), fetch_count(0),
+				reader{read, this}
+{
+    for (int* i = id_list ; *i != -1 ; i++)
+    	cache_map[*i] = entry(*i);
+}
 
 cache::~cache()
 {
 	stop = true;
 	reader.join();
 	db_file.close();
-
-	for (int i = 0 ; i < cache_size ; i++)
-		if (!cached_entries[i])
-			delete cached_entries[i];
-
-	delete cached_entries;
 }
 
-cache::entry::entry() : location(new char[entry_size]),
+cache::entry::entry(int offset) : db_offset(offset), memory(nullptr)
 {
-	accessed = duration_cast<long>(system_clock::now());
+	accessed = system_clock::to_time_t(system_clock::now());
 }
 
-cache::entry::~entry() {}
-
-char* cache::entry::access()
+cache::entry::~entry()
 {
-	accessed = duration_cast<long>(system_clock::now());
-	return location;
+	if (memory)
+		delete[] memory;
+}
+
+cache::entry::operator=(entry&& in)
+{
+	db_offset	= in.db_offset;
+	memory		= in.memory.load(std::memory_order_seq_cst);
+	accessed	= in.accessed;
+}
+
+bool cache::query::operator()(query& a, query& b)
+{
+	return (a.immediate && !b.immediate);
+}
+
+unsigned cache::get_num_fetches()
+{
+	return fetch_count;
+}
+
+char* cache::operator[](int entry_id)
+{
+	entry* cur_entry = &cache_map[entry_id];
+
+	cur_entry->lock.lock();
+	cur_entry->accessed = system_clock::to_time_t(system_clock::now());
+
+	if (cur_entry->memory == nullptr)
+	{
+		cur_entry->lock.unlock();
+
+		queue_lock.lock();
+        read_queue.push(query(entry_id, true));
+
+        sleep.notify_all();
+
+        if (line_length > 1)
+		{
+			auto itr = next(cache_map.find(entry_id));
+			for (int i = 1 ; i < line_length ; i++)
+			{
+				read_queue.push(query(itr->first, false));
+				itr++;
+			}
+			sleep.notify_all();
+		}
+		queue_lock.unlock();
+
+		cur_entry->lock.lock();
+	}
+
+	cur_entry->lock.unlock();
+
+	return cur_entry->memory;
 }
 
 void cache::read()
 {
-	bool flag = !stop;
-	
-	while (flag)
+	slock lock(sleep_lock);
+
+	while (!stop)
 	{
 		while (!read_queue.empty())
 		{
 			// Get next thing off of the queue
 			queue_lock.lock();
-			
-			int id(read_queue.front());
+
+			int id(read_queue.top().id);
 			read_queue.pop();
-			
+
 			queue_lock.unlock();
-			
-			// Fetch an entry from disk
-			entry new_entry;
-			fetch(id, new_entry.location);
-			
-			// Write entry to cache map
-			cache_lock.lock();
-			
-			if (cache_map.size() == cache_size())
+
+			if (cache_map[id].memory == nullptr)
 			{
-				// Remove least recently used
-				auto lru(cache_map.begin());
-				for (auto i = cache_map.begin() ; i != cache_map.end() ; i++)
-					if ((*lru).accessed < (*i).accessed)
-						lru = i;
-						
-				cache_map.remove(lru);
+				cache_map[id].lock.lock();
+
+				// If the cache is full
+				if (cache_map.size() == cache_size)
+				{
+					// Remove least recently used
+					// TODO: Garbage collection
+				}
+
+				// Fetch an entry from disk
+				char* new_block = new char[entry_size];
+				cache_map[id].memory = new_block;
+				fetch(cache_map[id].db_offset, new_block);
+
+				cache_map[id].lock.unlock();
 			}
-			
-			// Entry
-			cache_map[id] = new_entry;
-			
-			cache_lock.unlock();
 		}
+
+		sleep.wait(lock);
 	}
 }
 
+void cache::fetch(int offset, char* put_here)
+{
+	db_file.seekg(offset, db_file.beg);
+	db_file.read(put_here, entry_size);
+}
--- cache.h
+++ cache.h
@@ -1,18 +1,23 @@
+#include <atomic>
 #include <condition_variable>
+#include <ctime>
 #include <fstream>
 #include <mutex>
 #include <queue>
 #include <string>
 #include <thread>
 #include <unordered_map>
+#include <vector>
 
 class cache
 {
 	public:
 		cache(std::unordered_map<int,int>& mapper, std::string& db_location,
-				unsigned entry_length, unsigned entries_per_line,
+				int* id_list, unsigned entry_length, unsigned entries_per_line,
 				unsigned num_lines, unsigned num_threads);
 		~cache();
+
+		unsigned get_num_fetches();
 
 		char* operator[](int entry_id);
 
@@ -20,20 +25,35 @@ class cache
 		class entry
 		{
 			public:
-				cached_entry();
-				~cached_entry();
-				char* access();
-			
-				char*	location;
-				long	accessed;
-		}
-		
+				entry() {}
+				entry(int offset);
+				operator=(entry&& in);
+				~entry();
+
+				int					db_offset;
+				std::atomic<char*>	memory;
+				std::time_t			accessed;
+				std::mutex			lock;
+		};
+
+		struct query
+		{
+			public:
+				query() {}
+				query(int id, bool now)
+				{
+					this -> id	= id;
+					immediate	= now;
+				}
+                int		id;
+                bool	immediate;
+                bool	operator()(query& a, query& b);
+		};
+
 		void read();
 		void fetch(int offset, char* put_here);
 
-		std::unordered_map<int, int>	db_mapper;		// Maps index->offset
-		std::unordered_map<int, entry>	cache_map;		// Maps index->cache
-		std::string						db_loc;			// Location of database
+		std::unordered_map<int, entry>	cache_map;		// Maps index->entry
 		std::ifstream					db_file;		// The database
 		unsigned						entry_size;		// Size of each entry
 		unsigned						line_length;	// Entries per line
@@ -42,11 +62,12 @@ class cache
 		unsigned						cache_size;		// Num entries in cache
 		unsigned						fetch_count;	// Number of fetches
 
-		std::queue				read_queue;	// Things to read
+		std::priority_queue<query, std::vector<query>, query>	read_queue;	// Things to read later
+
 		std::mutex				queue_lock;	// Queue lock
 		std::mutex				cache_lock;	// Cache lock
-		std::mutex				file_lock;	// File lock
 		std::thread				reader;		// Reading thread
 		std::condition_variable	sleep;		// Sleep variable
-		bool					stop;		// Stop flag
+		std::mutex				sleep_lock;	// Sleep lock
+		std::atomic_bool		stop;		// Stop flag
 };
